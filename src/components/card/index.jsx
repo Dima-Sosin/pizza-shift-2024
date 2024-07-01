8e4d92110e7570cdb237df1488b540724e0fd104
Dima-Sosin <sosindima5@gmail.com>
2024-07-01 22:23:37 +0700

    Сделал модальное окно

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,16 +1,35 @@
 import styles from "./styles.module.css"
 import { baseUrl } from "../../rest/index.js"
 import { Button } from "../button/index.jsx"
+import { useEffect, useState } from "react"
+import { Modal } from "../modal/index.jsx"
 
+{/* eslint-disable react/prop-types */}
 export const Card = ({pizza}) => {
+    const[isModal, setIsModal] = useState(false)
 
+    useEffect(() => {
+        if(isModal){
+            document.body.style.overflow = 'hidden';
+        }
+        else{
+            document.body.style.overflow = 'unset';
+        }
+    }, [isModal])
     return(
-        <div className={styles.card}>
-            <img className={styles.img} src={baseUrl + pizza.img} alt="poster" loading="lazy"/>
-            <h3 className={styles.title}>{pizza.name}</h3>
-            <p className={styles.description}>{pizza.description}</p>
-            <p className={styles.price}>от {pizza.sizes[0].price} ₽</p>
-            <Button>Подробнее</Button>
-        </div>
+        <>
+            <div className={styles.card} onClick={() => setIsModal(true)}>
+                <img className={styles.img} src={baseUrl + pizza.img} alt="poster" loading="lazy"/>
+                <h3 className={styles.title}>{pizza.name}</h3>
+                <p className={styles.description}>{pizza.description}</p>
+                <p className={styles.price}>от {pizza.sizes[0].price} ₽</p>
+                <Button>Подробнее</Button>
+            </div>
+            {isModal &&
+                <Modal setIsModal={setIsModal}>
+                    <h2>{pizza.name}</h2>
+                </Modal>
+            }
+        </>
     )
 }
---
+++ src/components/modal/index.jsx
@@ -0,0 +1,18 @@
+import styles from "./styles.module.css"
+
+// eslint-disable-next-line react/prop-types
+export const Modal = ({children, setIsModal}) => {
+
+    return(
+        <div className={styles.modal}>
+            <div className={styles.modal_content}>
+                <div className={styles.close} onClick={() => setIsModal(false)}>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="transparent" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M21.085 3.53a.75.75 0 0 0-1.06 0l-7.719 7.718-7.71-7.711a.75.75 0 1 0-1.06 1.06l7.71 7.712-7.716 7.716a.75.75 0 1 0 1.061 1.061l7.715-7.717 7.726 7.725a.75.75 0 0 0 1.06-1.06l-7.724-7.725 7.717-7.719a.75.75 0 0 0 0-1.06" fill="currentColor"/>
+                    </svg>
+                </div>
+                {children}
+            </div>
+        </div>
+    )
+}
