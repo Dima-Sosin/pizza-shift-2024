7371ea86997acf4a14c7c9ea0ddc2c80aee8411d
Dima-Sosin <sosindima5@gmail.com>
2024-07-07 15:28:30 +0700

    Переписал react-router-dom на 6-ую версию

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,41 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { App } from './App.jsx'
-import { store } from "./store/store"
+import store from "./store/store"
 import { Provider } from "react-redux"
-import { BrowserRouter } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { CatalogPage } from "./pages/catalog-page.jsx"
+import { ProfilePage } from "./pages/profile-page.jsx"
+import { OrdersPage } from "./pages/orders-page.jsx"
+import { PageHeader } from "./components/page-header/index.jsx"
+import { CartPage } from "./pages/cart-page.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-      <Provider store={store}>
-          <App />
-      </Provider>
-  </BrowserRouter>,
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <PageHeader />,
+        children: [
+            {
+                path: "catalog",
+                element: <CatalogPage />,
+            },
+            {
+                path: "profile",
+                element: <ProfilePage />,
+            },
+            {
+                path: "orders",
+                element: <OrdersPage />
+            },
+            {
+                path: "cart",
+                element: <CartPage />
+            }
+        ]
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <RouterProvider router={router} />
+    </Provider>
 )
